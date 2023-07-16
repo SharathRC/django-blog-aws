6e63a9a824d4f99748c6a1eb5c57e3b462a4d724
Sharath RC <sharathr.sh@gmail.com>
2023-07-16 17:45:51 +0200

    add navbar

--- DjangoBlog/Article/urls.py
+++ DjangoBlog/Article/urls.py
@@ -1,8 +1,8 @@
 from django.urls import path
-from .views import opening_page, article_list
+from .views import opening_page, articles_list
 
 
 urlpatterns = [
     path('', opening_page, name='opening_page'),
-    path('articles/', article_list, name='article_list'),
+    path('articles/', articles_list, name='article_list'),
 ]
--- DjangoBlog/Article/views.py
+++ DjangoBlog/Article/views.py
@@ -1,9 +1,10 @@
 from django.shortcuts import render, HttpResponse
+from .models import Article
 
 
 def opening_page(request):
     return render(request, 'opening_page.html')
 
-def article_list(request):
-    articles = "New article"
-    return render(request, 'articles.html', {'articles': articles})
+def articles_list(request):
+    articles_list = Article.objects.all().order_by('-published')
+    return render(request, 'articles.html', {'articles_list': articles_list})
