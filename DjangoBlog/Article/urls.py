1d8521045d7e69a8c83708880f2cf6bb83c0cf90
Sharath RC <sharathr.sh@gmail.com>
2023-07-16 18:41:57 +0200

    adding user account code

--- DjangoBlog/Article/urls.py
+++ DjangoBlog/Article/urls.py
@@ -1,8 +1,9 @@
 from django.urls import path
-from .views import opening_page, articles_list
+from .views import opening_page, articles_list, articles_details
 
 
 urlpatterns = [
     path('', opening_page, name='opening_page'),
     path('articles/', articles_list, name='article_list'),
+    path('articles/<slug:slug>/', articles_details, name='article_details'),
 ]
--- DjangoBlog/Article/views.py
+++ DjangoBlog/Article/views.py
@@ -1,5 +1,6 @@
-from django.shortcuts import render, HttpResponse
+from django.shortcuts import render, HttpResponse, get_object_or_404
 from .models import Article
+from .forms import UserRegistrationForm
 
 
 def opening_page(request):
@@ -8,3 +9,24 @@ def opening_page(request):
 def articles_list(request):
     articles_list = Article.objects.all().order_by('-published')
     return render(request, 'articles.html', {'articles_list': articles_list})
+
+def articles_details(request, slug):
+    article = get_object_or_404(Article, slug=slug)
+    return render(request, 'details.html', {'article': article})
+
+def register(request):
+    if request.method == 'POST':
+        user_form = UserRegistrationForm(request.POST)
+
+        if user_form.is_valid():
+            new_user = user_form.save(commit=False)
+            new_user.set_password(user_form.cleaned_data['password'])
+            
+            new_user.save()
+            
+            return render(request, 'account/register_done.html', {'user_form': user_form})
+    else:
+        user_form = UserRegistrationForm()
+        
+    return render(request, 'account/register.html', {'user_form': user_form})
+        
