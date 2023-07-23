1550a6b1f48df567a6dab4e0af1a21e6482e876e
Sharath RC <sharathr.sh@gmail.com>
2023-07-23 18:55:30 +0200

    add login control

--- DjangoBlog/Article/views.py
+++ DjangoBlog/Article/views.py
@@ -1,4 +1,6 @@
 from django.shortcuts import render, HttpResponse, get_object_or_404, redirect
+from django.contrib.auth.decorators import login_required
+
 from .models import Article
 from .forms import UserRegistrationForm, ArticleAddingForm, UpdateArticle
 
@@ -6,10 +8,12 @@ from .forms import UserRegistrationForm, ArticleAddingForm, UpdateArticle
 def opening_page(request):
     return render(request, 'opening_page.html')
 
+@login_required
 def articles_list(request):
     articles_list = Article.objects.all().order_by('-published')
     return render(request, 'articles.html', {'articles_list': articles_list})
 
+@login_required
 def articles_details(request, slug):
     article = get_object_or_404(Article, slug=slug)
     return render(request, 'details.html', {'article': article})
@@ -30,6 +34,7 @@ def register(request):
         
     return render(request, 'account/register.html', {'user_form': user_form})
 
+@login_required
 def article_form(request):
     article_form = ArticleAddingForm(request.POST)
     
@@ -45,6 +50,7 @@ def article_form(request):
         
     return render(request, 'account/add_article.html', {'article_form': article_form})
 
+@login_required
 def update_article(request, slug):
     article = get_object_or_404(Article, slug=slug)
     update_article_form = UpdateArticle(request.POST or None, instance=article)
@@ -58,12 +64,13 @@ def update_article(request, slug):
         
     return render(request, 'account/update_article.html', {'update_article_form': update_article_form, 'article': article})
 
+@login_required
 def delete_article(request, slug):
     article = get_object_or_404(Article, slug=slug)
     
     if article:
         article.delete()
-        return render(request, 'account/delete_article.html', {'article': article})
+        return render(request, 'account/delete_article.html')
     
     return render()
         
